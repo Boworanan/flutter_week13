a72b48e9ea9f95040f0027cc5c118a146342124c
Boworanan <bowonranan.30@gmail.com>
2022-08-11 16:56:43 +0700

    update

--- lib/gridview_card_page.dart
+++ lib/gridview_card_page.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
+class Menu{
+  final String? title;
+  final IconData? icon;
+  final Color? bgcolor;
+  Menu({this.title,this.icon,this.bgcolor});
+}
+
+List<Menu> menus = [
+  Menu(title: "Menu 1",icon: Icons.apple,bgcolor: Colors.redAccent),
+  Menu(title: "Menu 2",icon: Icons.person,bgcolor: Colors.yellowAccent),
+  Menu(title: "Menu 3",icon: Icons.android,bgcolor: Colors.greenAccent),
+];
+
 class GridViewCrad extends StatelessWidget{
   const GridViewCrad({Key? key}):super(key: key);
   @override
@@ -9,13 +22,13 @@ class GridViewCrad extends StatelessWidget{
       body: Container(
         padding: EdgeInsets.all(8),
         child: GridView.count(
-          crossAxisCount: 3,
+          crossAxisCount: menus.length,
           mainAxisSpacing: 20,
           crossAxisSpacing: 20,
           //children: List.generate(9, (index) => Card()),
-          children: List.generate(9,(index){
+          children: List.generate(menus.length,(index){
             return Card(
-              color: Colors.greenAccent,
+              color: menus[index].bgcolor,
               elevation: 2,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.all(
@@ -29,8 +42,8 @@ class GridViewCrad extends StatelessWidget{
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    Icon(Icons.person,size: 65,color: Colors.white,),
-                    Text("Menu 1",style: TextStyle(color: Colors.white))
+                    Icon(menus[index].icon,size: 65,color: Colors.white,),
+                    Text(menus[index].title!,style: TextStyle(color: Colors.white))
                   ],
                 ),
                 onTap: (){},
